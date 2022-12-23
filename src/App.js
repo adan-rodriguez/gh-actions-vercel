1872fbcb430231b313c234bef3afae1e9ddc7e98
Adan Rodriguez <adan.rodriguez.fusta@gmail.com>
2022-12-23 17:09:08 -0300

    Add 'Adán Rodríguez' to homepage

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Adán Rodríguez</h1>
         <p>
           GH Actions - Deploy to Vercel
         </p>
